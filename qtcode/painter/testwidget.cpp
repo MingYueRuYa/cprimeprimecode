b06b91b172f9a015989b93694428b5666fa1c167
MingYueRuYa <714049438@qq.com>
2015-11-26 21:57:57 +0800

    add files

---
+++ qtcode/painter/testwidget.cpp
@@ -0,0 +1,5 @@
+#include "testwidget.h"
+
+testwidget::testwidget()
+{
+}
---
+++ qtcode/painter/testwidget.h
@@ -0,0 +1,10 @@
+#ifndef TESTWIDGET_H
+#define TESTWIDGET_H
+
+class testwidget
+{
+public:
+    testwidget();
+};
+
+#endif // TESTWIDGET_H
