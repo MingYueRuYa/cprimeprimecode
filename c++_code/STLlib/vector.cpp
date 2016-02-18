6004c7bfc74c3a6d1615eb876c62681aa10ce7a0
MingYueRuYa <714049438@qq.com>
2016-02-18 20:51:02 +0800

    add content

--- c++_code/STLlib/vector.cpp
+++ c++_code/STLlib/vector.cpp
@@ -10,7 +10,6 @@ using std::vector;
 using std::cout;
 using std::endl;
 
-
 void print(const int &pNum)
 {
 	cout << pNum << endl;		
@@ -21,10 +20,13 @@ int main(int argc, char *argv[])
 	vector<int> intvector = {1, 2, 3};
 	for_each(intvector.begin(), intvector.end(), print);
 	vector<int> intvector01(10, -1);
-	for_each(intvector01.begin(), intvector01.end(), print);
+	//for_each(intvector01.begin(), intvector01.end(), print);
 	//vector<int> intvector02(intvector01.begin(), intvector01.end());
 	//for_each(intvector02.begin(), intvector02.end(), print);
 	vector<int> intvector03 = intvector;
-	for_each(intvector03.begin(), intvector03.end(), print);
+	//for_each(intvector03.begin(), intvector03.end(), print);
+	vector<int>::iterator begin = intvector.begin();
+	vector<int>::iterator insertiterator = intvector.insert(begin, 4);
+	cout << *insertiterator;
 	return 0;
 }
