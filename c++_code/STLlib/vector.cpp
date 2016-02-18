3ef007bdcc6c92f43dc2b07409f8b68938d5cf1e
MingYueRuYa <714049438@qq.com>
2016-02-18 23:17:32 +0800

    add vector content.

--- c++_code/STLlib/vector.cpp
+++ c++_code/STLlib/vector.cpp
@@ -5,17 +5,25 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <string>
 
 using std::vector;
 using std::cout;
+using std::cin;
 using std::endl;
+using std::string;
 
 void print(const int &pNum)
 {
 	cout << pNum << endl;		
 }
 
-int main(int argc, char *argv[])
+void printstr(const string &pNum)
+{
+	cout << pNum << endl;		
+}
+
+int main01(int argc, char *argv[])
 {
 	vector<int> intvector = {1, 2, 3};
 	for_each(intvector.begin(), intvector.end(), print);
@@ -30,3 +38,15 @@ int main(int argc, char *argv[])
 	cout << *insertiterator;
 	return 0;
 }
+
+int main(int argc, char *argv[])
+{
+	vector<string> vectorstring;
+	string inputstr;
+	while (cin >> inputstr) {
+		vectorstring.push_back(inputstr);
+	}
+	for_each(vectorstring.begin(), vectorstring.end(), printstr);
+	return 0;
+}
+
