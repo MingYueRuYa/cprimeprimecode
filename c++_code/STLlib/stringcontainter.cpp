3a826191e160e58660e454087805790ffa34443d
MingYueRuYa <714049438@qq.com>
2016-02-18 23:18:08 +0800

    add string contents.

--- c++_code/STLlib/stringcontainter.cpp
+++ c++_code/STLlib/stringcontainter.cpp
@@ -9,7 +9,7 @@
  */
 #include <iostream>
 #include <string>
-
+#include <algorithm>
 
 using namespace std;
 int main01()
@@ -41,12 +41,97 @@ int main01()
 	return 0;
 }
 
-int main(void)
+int main02(void)
 {
 	string tmpstring("hello string");	
 	for (string::size_type index = 0; index < tmpstring.length(); ++index) {
 		cout << tmpstring[index] << " ";
 	}
 	cout << endl;
+	string findstring("linux windows");
+	string::size_type pos = findstring.find("linux2");
+	if (string::npos == pos) {
+		cout << "Not find substring." << endl;
+	} else {
+		cout << pos << endl;
+	}
+	string::iterator positerator = find(findstring.begin(), findstring.end(), 'l');
+	if (positerator != findstring.end()) {
+		cout << *positerator << endl;
+	}
+	return 0;
+}
+
+//c string 与 c++ string 相互转换
+int main03(void)
+{
+	const char *cstring = "cstring";
+	string strstr(cstring);
+	cout << strstr << endl;
+	cout << strstr.c_str() << endl;
+	return 0;
+}
+
+//字符串 增加
+int main04(void)
+{
+	string addstring("hello");
+	addstring += " world";
+	cout << addstring << endl;
+	addstring.insert(5, " linux");
+	cout << addstring << endl;
+	addstring.append(" unix");
+	cout << addstring << endl;
+	return 0;
+}
+
+int main05(void)
+{
+	string daystring = "today is good day!";	
+	string::size_type pos = daystring.find("day");
+	while (string::npos != pos) {
+		cout << "find pos is " << pos << endl;
+		++pos;
+		pos = daystring.find("day", pos);
+	}
+	return 0;
+}
+
+//删除string
+int main06(void)
+{
+	string hellostring = "hellostring linux world";
+	hellostring.erase(10, 5);
+	cout << hellostring << endl;
+	string::iterator deliterator = find(hellostring.begin(), hellostring.end(), 'l');
+	while (deliterator != hellostring.end()) {
+		hellostring.erase(deliterator);
+		deliterator = find(hellostring.begin(), hellostring.end(), 'l');
+
+	}
+	cout << hellostring << endl;
+	return 0;
+}
+
+int to_upper(int c)
+{
+	if (islower(c)) {
+		return (c - 32);
+	}
+	return c;
+}
+
+//关于string 算法
+int main(void)
+{
+	string reversestring = "reversestring";
+	reverse(reversestring.begin(), reversestring.end());
+	//cout << reversestring << endl;
+
+	string strinput("");
+	string output("");
+	getline(cin, strinput);	
+	transform(strinput.begin(), strinput.end(), strinput.begin(), to_upper);
+	cout << strinput << endl;
 	return 0;
 }
