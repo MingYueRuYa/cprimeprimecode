e6da01e43760c4210526201b5755599018686ef2
MingYueRuYa <714049438@qq.com>
2016-03-16 13:16:26 +0800

    add read from stdin shell.

---
+++ c++_code/linux/shell_read.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo -n "FirstName:"
+read firstname
+
+echo -n "LastName:"
+read lastname lastname2
+
+echo -e "FistName:${firstname}.\n"
+echo -e "LastName:${lastname} ${lastname2}.\n"
