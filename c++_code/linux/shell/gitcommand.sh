2d10ab08ed86eacb5b01dbe819c58f4e05c5fe00
MingYueRuYa <714049438@qq.com>
2016-03-18 13:27:24 +0800

    modify git shell.

--- c++_code/linux/shell/gitcommand.sh
+++ c++_code/linux/shell/gitcommand.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+# this sh write by liushixiong
+# main purpose to operate git command
+# 2016/03/18 13:25
 
-sudo git add $1
-echo $1
+sudo git add "$1"
 sudo git commit -m "$2"
 sudo git push
