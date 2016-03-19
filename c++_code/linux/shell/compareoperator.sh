3e153b1892bd0d1a45fadd5ebfdd748964ae4547
MingYueRuYa <714049438@qq.com>
2016-03-19 23:58:15 +0800

    operator sh.

---
+++ c++_code/linux/shell/compareoperator.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#test txt file
+#test -f  testfile0.txt
+#
+#if [ $? -eq "0" ]; then
+#	echo -e "success.\n"
+#else
+#	echo -e "fial.\n"
+#fi
+
+#test string 
+# -z 测试字符串是否为空， =两个是否相等，!=不相等
+str="linux"
+str1="linux"
+#if [ -z $str  ]; then
+#	echo "str is null."
+#else 
+#	echo $str
+#fi
+
+#if [ $str1=$str ]; then
+#	echo "str and str1 is equal."
+#else 
+#	echo "Two string is not equal."
+#fi
+
+#注意测试数字的时候使用 -eq -ne -gt -lt -ge -lt
