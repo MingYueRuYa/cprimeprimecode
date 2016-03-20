5a38d6276a40492e8a45a5ccc283ec3c018f53d5
MingYueRuYa <714049438@qq.com>
2016-03-20 11:12:25 +0800

    update git command sh.

--- c++_code/linux/shell/gitcommand.sh
+++ c++_code/linux/shell/gitcommand.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 # this sh write by liushixiong
 # main purpose to operate git command
-# 2016/03/18 13:25
-
-sudo git add "$1"
-sudo git commit -m "$2"
-sudo git push
+# 										2016/03/18 13:25
+# add if contament 
+#										2016/03/19 21:54
+#echo "$1"
+#[ "$1" = "pull" ]
+#echo $?
+if [ "$1" = "pull" ]; then
+	sudo git pull
+	echo "hello"
+else
+	#git status > status.txt | grep "modified" | awk -F" " '{print $2}' 
+	#not complete.
+	sudo git add "$1"
+	sudo git commit -m "$2"
+	sudo git push
+fi
