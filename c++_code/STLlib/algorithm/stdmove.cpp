1e0d4e7e0535df1a4d4b9ddb43c5f0c144c6deeb
MingYueRuYa <714049438@qq.com>
2016-03-27 15:05:50 +0800

    modified stdmove.cpp .

--- c++_code/STLlib/algorithm/stdmove.cpp
+++ c++_code/STLlib/algorithm/stdmove.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-int main(void)
+int main01(void)
 {
 	int var1 = 10;
 	int &var2 = var1;
@@ -15,12 +15,41 @@ int main(void)
 	cout << var1 << endl;
 	//int &&var3 = 3;
 	int &&var3 = std::move(var1);
-	cout << var3 << endl;
-	cout << "var1 " << var1 << endl;
+	cout << "var3 " << &var3 << " var1 " << &var1; //var3 address is equial var1
+//	cout << var3 << endl;
+//	cout << "var1 " << var1 << endl;
 	//int &var4 = var2 + 20; //error
 	//const int var5 = 20; //error
 	//int &var4 = var5;
 	const int &var4 = 4;
-	cout << "var1 " << var1 << endl;
+	//cout << "var1 " << var1 << endl;
+	int *i1 = new int(10);
+	int *&& i2 = NULL;
+	i2 = std::move(i1);
+	delete i1;
+	i1 = 0x0000000;
+	cout << " i2 " << *i2 << endl;
 	return 0;
 }
+
+typedef struct __Teacher
+{
+	char *name;
+	int age;
+}Teacher;
+
+int main(void)
+{
+	Teacher *teacher = new Teacher();
+	teacher->name = "linux";
+	teacher->age = 30;
+	Teacher *&&t1 = std::move(teacher);
+	delete teacher;
+	printf("%0x, %0x", teacher, t1);
+	cout << t1->name << "  " << t1->age << endl;
+	return 0;
+}
+
+
+
+
