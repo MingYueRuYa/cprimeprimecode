261790a96a1a8a40334c44278915d48d11009cd1
MingYueRuYa <714049438@qq.com>
2016-04-16 16:00:59 +0800

    multiprocess

--- c++_code/linux/socket/echo/echoclient.c
+++ c++_code/linux/socket/echo/echoclient.c
@@ -3,60 +3,79 @@
 #include <unistd.h>
 #include <errno.h>
 #include <signal.h>
+#include <string.h>
 #include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <sys/socket.h>
 
 #define PORT 8001
 #define SERVER_IP "127.0.0.1"
 
+int global_sockfd;
+
+void SigHandler(int pSignal, siginfo_t *pSiginfo, void *pVoid)
+{
+	if (SIGINT == pSignal) {
+		close(global_sockfd);
+		printf("Close client.\n");
+		exit(0);
+	}
+}
+
 int main(void)
 {
-	int sockfd;
-	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+	struct sigaction act;
+	act.sa_sigaction = SigHandler;
+	act.sa_flags = SA_SIGINFO;
+	sigaction(SIGINT, &act, NULL);
+	if ((global_sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
 		perror("Connect error:\n");
 		exit(-1);
 	}
-	struct sockaddr_in sockaddr;
-	sockaddr.sin_family = AF_INET;
-	sockaddr.sin_port = htons(PORT);
-	sockaddr.sin_addr.s_addr = inet_addr(SERVER_IP);
-	if (connect(sockfd, &sockaddr, sizeof(sockaddr)) < 0 ) {
+	struct sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(PORT);
+	addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+	if (connect(global_sockfd, (struct sockaddr *)&addr, sizeof(addr)) < 0 ) {
 		perror("Connect error:\n");
 		exit(-1);
 	}
+	/*
 	struct sockaddr_in localaddr;	
-	sizelen_t socklen = sizeof(localaddr);
+	socklen_t socklen = sizeof(localaddr);
 	if (getsockname(sockfd, (struct sockaddr *)&localaddr, &socklen) < 0) {
 		perror("getsockname error:\n");
 	}
+	*/
 	
 	char sendbuf[1024] = {0};
 	char recvbuf[1024] = {0};
 	while (fgets(sendbuf, sizeof(sendbuf), stdin)) {
-		int writelen = write(sockfd, sendbuf, strlen(sendbuf));
+		int writelen = write(global_sockfd, sendbuf, strlen(sendbuf));
 		if (-1 == writelen) {
 			if (EINTR == errno) {
 				continue;
 			}
-			return;
+			return -1;
 		}
-		int readlen = read(sockfd, recvbuf, sizeof(recvbuf));
+		int readlen = read(global_sockfd, recvbuf, sizeof(recvbuf));
 		if (0 == readlen) {
 			printf("Server close.\n");
-			close(sockfd);
+			close(global_sockfd);
 		}
 		else if (-1 == readlen) {
-			if (EINTR == errono) { //maybe interupte by signal
+			if (EINTR == errno) { //maybe interupte by signal
 				continue;
 			}
 			perror("Write error:\n");
-			return;
+			return -1;
 		}
 		else {
-			printf("%s.\n", recvbuf);
+			printf("%s", recvbuf);
 		}
 		memset(sendbuf, 0, sizeof(sendbuf));
-		memset(recvbuf, 0, sizeof(recvbuf);
+		memset(recvbuf, 0, sizeof(recvbuf));
 	}
 	return 0;
 }
--- c++_code/linux/socket/echo/echoserver.c
+++ c++_code/linux/socket/echo/echoserver.c
@@ -3,16 +3,32 @@
 #include <unistd.h>
 #include <errno.h>
 #include <signal.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/wait.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#define SERVER_IP 127.0.0.1
+#define SERVER_IP "127.0.0.1"
 #define SERVER_PORT 8001
+
+void SigchldHandler(int pSignal)
+{
+	int status;
+	int pid = waitpid(-1,&status, WNOHANG);
+	if (-1 == pid) {
+		perror("Wait process error:\n");
+		return;
+	}
+	else if (pid > 0) {
+		printf("wait process pid is %d.\n", pid);
+	}
+}
 
 int main(void)
 {
+	signal(SIGCHLD, SigchldHandler);
 	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (-1 == sockfd) {
 		perror("Socket error:\n");
@@ -22,7 +38,11 @@ int main(void)
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(SERVER_PORT);
 	addr.sin_addr.s_addr = inet_addr(SERVER_IP);
-	if (-1 == bind(sockfd, (struct sockaddr *)addr, sizeof(addr))) {
+	int optval = 1;
+	if (-1 == setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval))) {
+		perror("Set socket option error:\n");
+	}
+	if (-1 == bind(sockfd, (struct sockaddr *)&addr, sizeof(addr))) {
 		perror("Bind error:\n");
 		return -1;
 	}
@@ -32,14 +52,57 @@ int main(void)
 	}
 	while (1) {
 		struct sockaddr_in clientaddr;
-		socklen_t socklen = 0;
+		socklen_t socklen = sizeof(clientaddr);
 		int connfd = accept(sockfd, (struct sockaddr *)&clientaddr, &socklen);
-		if (-1 == connfd) {
-			perror("Accept error:\n");
-			return -1;
+		pid_t pid = fork();
+		if (-1 == pid) {
+			perror("Create process error:\n");
+			exit(-1);	
+		}
+		else if (0 == pid) { //child process
+			if (-1 == connfd) {
+				perror("Accept error:\n");
+				return -1;
+			}
+			printf("child process.\n");
+			printf("client address %s.\n", inet_ntoa(clientaddr.sin_addr));
+			while (1) {
+				char recbuffer[1024] = {0};
+				int readlen = read(connfd, recbuffer, sizeof(recbuffer));	
+				if (-1 == readlen) {
+					if (EINTR == errno) {
+						continue;
+					}
+					//return - 1;
+					exit(-1);
+				}
+				else if (0 == readlen) {
+					printf("Client close.\n");
+					close(connfd);
+					exit(0);
+				}
+				else {
+					int writelen = write(connfd, recbuffer, strlen(recbuffer));
+					if (-1 == writelen) {
+						if (EINTR == errno) {
+							continue;
+						}
+						close(connfd);
+						exit(-1);
+					}
+					else if (writelen > 0) {
+						//TODO something
+					}
+					else {
+					}
+					memset(recbuffer, 0, sizeof(recbuffer));
+				}
+			}
+		}
+		else if (pid > 0) { //parent process
+			close(connfd);
 		}
 	}
-		
 	return 0;
 }
 
