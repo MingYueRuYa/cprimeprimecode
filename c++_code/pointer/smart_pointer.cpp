6652458e6fbc86c3dc610a3076fffea4ddb32b40
MingYueRuYa <714049438@qq.com>
2016-05-11 22:11:21 +0800

    complied shared_ptr seccussful and rename auotpointer file.

--- c++_code/pointer/smart_pointer.cpp
+++ c++_code/pointer/smart_pointer.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <memory>
-#include <tr1/memory>
+#include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
 
 using namespace std;
-using namespace std::tr1::shared_ptr;
+using namespace boost;
 
 int main(void)
 {
 	for (int i = 0; i < 10000; ++i) {
 		//unique_ptr<int>	int_ptr(new int());
-		shared_ptr<int> p;
+		//shared_ptr<int> p(new int(2));
+		shared_ptr<int> ptr = make_shared<int>(2);
 	}
 	return 0;
 }
