c407ce0a86c45bdea49afc12fe12ebda8cb0eb93
MingYueRuYa <714049438@qq.com>
2016-05-11 13:28:01 +0800

    add shared_ptr but complie error.

--- c++_code/pointer/autopointer.cpp
+++ c++_code/pointer/autopointer.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <memory>
+#include <tr1/memory>
 
 using namespace std;
+using namespace std::tr1::shared_ptr;
 
 int main(void)
 {
 	for (int i = 0; i < 10000; ++i) {
-		unique_ptr<int>	int_ptr(new int());
+		//unique_ptr<int>	int_ptr(new int());
+		shared_ptr<int> p;
 	}
 	return 0;
 }
