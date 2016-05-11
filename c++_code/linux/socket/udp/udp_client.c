b82398d58812ccb6c9f38656b164445fdc2985d4
MingYueRuYa <714049438@qq.com>
2016-05-11 22:24:45 +0800

    add udp module.

---
+++ c++_code/linux/socket/udp/udp_client.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 8002
+
+#define ERR_EXIT(m) \
+	do { \
+		perror(m); \
+		exit(EXIT_FAILURE); \
+	} while (0)
+
+void echo_cli(int sockfd)
+{
+	struct sockaddr_in servaddr;
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = inet_addr(SERVER_IP);
+	servaddr.sin_port = htons(SERVER_PORT);
+
+	int ret = 0;
+	char sendbuf[1024] = {0};
+	char recvbuf[1024] = {0};
+
+	while (NULL != fgets(sendbuf, sizeof(sendbuf), stdin)) {
+		if (sendto(sockfd, sendbuf, strlen(sendbuf), 0, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+			ERR_EXIT("sendto error:");
+		}
+		if (recvfrom(sockfd, recvbuf, sizeof(recvbuf), 0, NULL, NULL) < 0) {
+			if (EINTR == errno) {
+				continue;
+			}
+			ERR_EXIT("recvfrom error:");
+		}
+		fputs(recvbuf, stdin);
+		memset(sendbuf, 0 , sizeof(sendbuf));
+		memset(recvbuf, 0 , sizeof(recvbuf));
+	}
+	close(sockfd);
+}
+
+int main(void)
+{
+	int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sockfd < 0) {
+		ERR_EXIT("socket error:");
+	}
+	echo_cli(sockfd);
+	return 0;
+}
---
+++ c++_code/linux/socket/udp/udp_server.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 8002
+
+#define ERR_EXIT(m) \
+	do { \
+		perror(m); \
+		exit(EXIT_FAILURE); \
+	} while (0)
+
+void echo_srv(int sockfd)
+{
+	char recvbuf[1024] = {0};
+	struct sockaddr_in peeraddr;
+	socklen_t peerlen;
+	int n;
+	while (1) {
+		peerlen = sizeof(peeraddr);
+		memset(&peeraddr, 0, sizeof(peeraddr));
+		n = recvfrom(sockfd, recvbuf, sizeof(recvbuf), 0, (struct sockaddr *)&peeraddr, &peerlen);
+		if (-1 == n) {
+			if (EINTR == errno) {
+				continue;
+			}
+			ERR_EXIT("recvfrom  error:");
+		}
+		else if (n > 0) {
+			int ret = 0;	
+			fputs(recvbuf, stdout);
+			ret = sendto(sockfd, recvbuf, n, 0, (struct sockaddr *)&peeraddr, peerlen);
+		}
+	}
+	close(sockfd);
+}
+
+
+int main(void)
+{
+	int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sockfd < 0) {
+		ERR_EXIT("socket error:");	
+	}
+	struct sockaddr_in servaddr;
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(SERVER_PORT);
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	if (bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+		ERR_EXIT("bind error:");
+	}
+	echo_srv(sockfd);
+	return 0;
+}
