d6023bdfd75784cd675a54b03f1c163869cad3f0
MingYueRuYa <714049438@qq.com>
2016-05-12 23:26:17 +0800

    complete strblob code.

--- c++_code/pointer/strblob.cpp
+++ c++_code/pointer/strblob.cpp
@@ -1,1 +1,53 @@
-#incldue "strblob.h"
+#include "strblob.h"
+
+StrBlob::StrBlob()
+	: data(make_shared<vector<string>>())
+{
+
+}
+
+StrBlob::StrBlob(initializer_list<string> il)
+	: data(make_shared<vector<string>>(il))
+{
+
+}
+
+StrBlob::size_type StrBlob::size() const
+{
+	return data->size();
+}
+
+bool StrBlob::empty() const
+{
+	return data->empty();
+}
+
+void StrBlob::push_back(const string &pStr)
+{
+	data->push_back(pStr);
+}
+
+void StrBlob::pop_back()
+{
+	check(0, "pop_back empty on StrBlob");
+	data->pop_back();
+}
+
+string& StrBlob::front()
+{
+	check(0, "front on empty StrBlob");
+	return data->front();
+}
+
+string& StrBlob::back()
+{
+	check(0, "back on empty StrBlob");
+	return data->back();
+}
+
+void StrBlob::check(size_type i, const string &msg) const
+{
+	if (i >= data->size()) {
+		throw out_of_range(msg);
+	}
+}
--- c++_code/pointer/strblob.h
+++ c++_code/pointer/strblob.h
@@ -1,1 +1,44 @@
-#include <stdio.h>
+#ifndef strblob_h
+
+#define strblob_h
+
+#include <iostream>
+#include <memory>
+#include <string>
+#include <exception>
+#include <initializer_list>
+#include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
+
+using namespace std;
+using namespace boost;
+
+class StrBlob
+{
+public:
+	typedef vector<string>::size_type size_type;
+
+	StrBlob();
+	
+	StrBlob(initializer_list<string> il);
+
+	size_type size() const;
+
+	bool empty() const;
+
+	void push_back(const string &pStr);
+
+	void pop_back();
+
+	string& front();
+
+	string& back();
+
+private:
+	boost::shared_ptr<vector<string>>	data;
+
+	void check(size_type i, const string &msg) const;
+
+};
+
+#endif //strblob_h
