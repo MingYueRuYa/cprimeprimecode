b80965d377ea8b2b1a83ec05502512b970447c73
MingYueRuYa <714049438@qq.com>
2016-05-17 22:59:41 +0800

    add unique_ptr.

--- c++_code/pointer/main.cpp
+++ c++_code/pointer/main.cpp
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include <boost/shared_ptr.hpp>
+//#include <boost/unique_ptr.hpp>
 
 #include "strblob.h"
 
@@ -20,7 +21,7 @@ int main01(int argc, char *argv[])
 	return 0;
 }
 
-int main02(int argc, char *argv[])
+int main03(int argc, char *argv[])
 {
 	//int * const pint = new int(10) const; error
 	//int * const pint = const new int(10) ; error
@@ -44,7 +45,7 @@ void process(boost::shared_ptr<int> p1)
 
 }
 
-int main(int argc, char *argv[])
+int main02(int argc, char *argv[])
 {
 	int *p1 = new (nothrow)int(10);
 	//process(boost::shared_ptr<int>(p1));
@@ -66,6 +67,36 @@ int main(int argc, char *argv[])
 	else {
 		*p2 += 20;
 	}
+	return 0;
+}
+
+/********************************unique_shared*********************************/
+
+std::unique_ptr<int> CopyUnique(int pUnique)
+{
+	return std::unique_ptr<int>(new int(pUnique));
+}
+
+std::unique_ptr<int> CopyUnique(std::unique_ptr<int> pUnique)
+{
+	return pUnique;
+}
+
+int main(int argc, char *argv[])
+{
+	cout << "main..." << endl;
+	std::unique_ptr<int> uniqueint(new int(10));
+	unique_ptr<int> p1;
+	p1.reset(uniqueint.release());
+	//error don't coyp unique_ptr, but has tow special example one is as parameter, other is as return value. tow example has main feature is unique_ptr will by destory.
+	//p1 = uniqueint 
+	cout << *p1 << endl;
+	*p1 = 20;
+	p1 = CopyUnique(*p1);
+	//attention: this unique_ptr as parameter as temperoy variable will be destory.
+	//there will be happen memory leak.
+	p1 = CopyUnique(std::unique_ptr<int>(new int(30)));
+	cout << *p1 << endl;
 	return 0;
 }
 
