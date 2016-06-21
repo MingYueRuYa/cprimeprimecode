8892283495ef2263b3f25cdf8e8b643e8046ebf1
MingYueRuYa <714049438@qq.com>
2016-06-21 23:27:05 +0800

    add thirdlib.

---
+++ c++_code/linux/thirdlib/libevent/main.cpp
@@ -0,0 +1,16 @@
+/****************************************************************
+ **
+ **	Copyright (C) 2016 635672377@qq.com
+ ** All rights reserved.
+ **
+ ***************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	
+	return 0;
+}
