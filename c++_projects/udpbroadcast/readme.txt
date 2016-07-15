9c5b802879a1f33544b71c16a3d88a10f2b46b05
MingYueRuYa <714049438@qq.com>
2016-07-15 22:57:50 +0800

    add udp broadcost projects.

---
+++ c++_projects/udpbroadcast/readme.txt
@@ -0,0 +1,1 @@
+this projces is about udp broadcast.
---
+++ c++_projects/udpbroadcast/udp_server.cpp
@@ -0,0 +1,16 @@
+/****************************************************************
+ **
+ **	Copyright (C) 2016 635672377@qq.com
+ ** All rights reserved.
+ **
+ ***************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+int main(int argc, char *argv[])
+{
+	return 0;
+}
