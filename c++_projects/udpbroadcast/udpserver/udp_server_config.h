6e38826d61f0c7ca71e9956a35eaecf7e2b226f6
MingYueRuYa <714049438@qq.com>
2016-07-31 18:17:27 +0800

    add server config.h .

---
+++ c++_projects/udpbroadcast/udpserver/udp_server_config.h
@@ -0,0 +1,24 @@
+/****************************************************************
+ **
+ **	Copyright (C) 2016 635672377@qq.com
+ ** All rights reserved.
+ **
+ ***************************************************************/
+
+#ifndef udp_server_config_h
+
+#define udp_server_config_h
+
+using std::string;
+
+//open glog 
+#define OPEN_GLOG
+
+#define APPLICATION_NAME "udp_server"
+
+#define APPLICATION_VERSION "0.01"
+
+#define APPLICATION_COPYRIGHT "Copyright (C) 2016 635672377@qq.com"
+#define APPLICATION_ALL_RIGHTS_RESERVED "All rights reserved" 
+
+#endif //udp_server_config_h
