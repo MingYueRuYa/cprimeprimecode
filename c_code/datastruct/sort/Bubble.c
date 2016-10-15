4d5e12e9023f4621ecce65a088d8163b4ade0a1a
root <root@localhost.localdomain>
2016-10-15 21:11:04 +0800

    add comment to Bubble sort.

--- c_code/datastruct/sort/Bubble.c
+++ c_code/datastruct/sort/Bubble.c
@@ -4,7 +4,9 @@ int main(int argc, char *argv)
 {
 	int arr[5] = {2, 4, 5, 1, 3};
 	for (int i = 0; i < 5; ++i) {
+		//注意循环的次数 5 - i
 		for (int j = 0; j < 5 - i; ++j) {
+			//注意比较的对象，是相邻的位置
 			if (arr[j] < arr[j+1]) {
 				int temp = arr[j];
 				arr[j] = arr[j+1];
