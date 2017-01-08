03927e51e1989346eca18022c7690ab9263d72f7
MingYueRuYa <714049438@qq.com>
2017-01-08 22:54:06 +0800

    add variadic template code.

---
+++ c++_code/template/variadictemplate.cpp
@@ -0,0 +1,36 @@
+/****************************************************************
+ **
+ **	Copyright (C) 2016 635672377@qq.com
+ ** All rights reserved.
+ **
+ ***************************************************************/
+
+/*
+ * 可变的模板参数
+ * */
+#include <iostream>
+#include <bitset>
+
+using namespace std;
+
+void print()
+{
+}
+
+//notice:
+//这里递归打印模板的可变参数，先是一个个的打印，打印到最后没有参数的时候
+//会调用上面的没有参数的print函数。
+//可以看成是一个参数和一包参数
+template<typename T, typename... Types>
+void print(const T& pFirstArg, const Types &... pTypes)
+{
+	//sizeof...(pTypes)可以知道除了第一个参数，后面还有多少个参数。
+	cout << pFirstArg << "-------- " << sizeof...(pTypes) << endl; 
+	print(pTypes...);
+}
+
+int main(int argc, char *argv[])
+{
+	print(1, 2.0, bitset<16>(16), "hello world!!!");	
+	return 0;
+}
