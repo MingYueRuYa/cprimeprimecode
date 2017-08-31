8945394adc3c46f0657325390483751493442879
MingYueRuYa <635672377@qq.com>
2017-08-31 23:22:44 +0800

    improve memory manager code.

--- memory_manager.cpp
+++ memory_manager.cpp
@@ -28,6 +28,7 @@
 
 using namespace std;
 
+//------------------------------------------------------------------------------------------------
 namespace demo01
 {
 void test_primitives()
@@ -75,6 +76,7 @@ void test_primitives()
 }
 }
 
+//------------------------------------------------------------------------------------------------
 namespace demo02
 {
 class A
@@ -99,6 +101,7 @@ void test_call_ctor_directory()
 	A *pA = new A(1); //ctor. this=0x16d8048 id=1
 	cout << pA->id << endl; // 1
 	//! pA->A::A(3); //in VC6 : ctor, this0013FF60 id 3
+					 //in vs2013 : ctor, this0013FF60 id 3
 					 //in GCC : [Error] cannot call constructor ‘demo02::A::A’ directly
 
 	cout << pA->id << endl; 		//in VC6 : 3
@@ -124,9 +127,157 @@ void test_call_ctor_directory()
 }
 }
 
+//------------------------------------------------------------------------------------------------
+//placement new
+#include <new>
+namespace demo03
+{
+using demo02::A;
+void test_array_new_and_placement_new()
+{
+	cout << "\ntest_placement_new()............... \n";
+	size_t size = 3;
+{
+//case 1
+//模拟memory pool的做法，array new + placement new，崩溃
+	A *buf  = (A *)(new char[sizeof(A)*size]);
+	A *temp = buf;
+	cout << "buf=" << buf << " temp=" << temp << endl;
+
+	for (int i=0; i<size; ++i) {
+		new(temp++) A(i);	//3次 ctor
+	}
+	//! delete [] buf; //crash . why?
+					   //因为这其实个char array, 看到 delete [] buf; 编译器会企图唤起多次A::~A()
+					   //但array memory layout中找到与array元素（本例3）相关的信息
+					   //--整个格局都是错乱（从我对VC的认识而言），于是崩溃
+					   //error message:*** Error in `./memory_manager': free(): invalid next size (fast): 0x01735040 ***
+	delete buf;		   //dtor just one time3, ~[0]
+}
+{
+//case 2
+//测试单纯的array new
+	A *buf = new A[size]; //default ctor 3 times, [0]->[1]->[2]
+						  //A必须有default ctor，否则[Error] no matching function for call to 'demo02::A::A()'
+	A *temp = buf;
+	cout << "buf=" << buf << " temp=" << temp << endl;
+
+	for (int i=0; i<size; ++i) {
+		new(temp++) A(i);	//3次 ctor
+	}
+	cout << "buf=" << buf << " temp=" << temp << endl;
+	delete [] buf;		 //dtor three times (次序逆反，[2]->[1]->[0])
+}
+{
+//case 3
+//掌握崩溃的原因，再次模拟memory pool做法，array new + placement new.
+//不用做了，因为memory pool只是提供memory，它并不管construction，
+//也不管destruction，它只负责回收memory.
+//所以它是以void *或char *取得memory，释放的也是void * or char *.
+//不像本例case 1 释放的是A*
+//
+//事实上 memory pool 形式如demo04::test
+}
+}
+}
+
+//------------------------------------------------------------------------------------------------
+namespace demo04
+{
+//ref. C++Primer 3/e, p.765
+//per-class allocator
+class Screen{
+public:
+	Screen(int x) : i(x) { };
+	int get() { return i; }
+
+	void *operator new(size_t);
+	void operator delete(void *, size_t);	//(2)
+	void operator delete(void *);			//(1) 二选一
+	//(1),(2)同时并存，在其他编译器下可能会爆出奇怪的错误信息，在pi os下没有出现
+
+private:
+	Screen *next;
+	static Screen *freeStore;
+	static const int screenChunk;
+private:
+	int i;
+};	
+Screen *Screen::freeStore = NULL;
+const int Screen::screenChunk = 24;
+
+void *Screen::operator new(size_t size)
+{
+	Screen *p;
+	if (!freeStore) {
+		//linked list 是空的，所以获取一大块memory
+		//以下调用的是global operator new
+		size_t chunk = screenChunk*size;
+		freeStore = p = reinterpret_cast<Screen *>(new char[chunk]);
+		//将分配得来的一大块memory，当做linked list将小块小块串联起来
+		for (; p != &freeStore[screenChunk-1]; ++p) {
+			p->next = p+1;
+		}
+		p->next = NULL;
+	}
+	p = freeStore;
+	freeStore = freeStore->next;
+	return p;
+}
+
+void Screen::operator delete(void *p, size_t)
+{
+	//将deleted object 收回插入linked list前端
+	(static_cast<Screen *>(p))->next = freeStore;
+	freeStore = static_cast<Screen *>(p);
+}
+
+void Screen::operator delete(void *p)
+{
+	//将deleted object 收回插入linked list前端
+	(static_cast<Screen *>(p))->next = freeStore;
+	freeStore = static_cast<Screen *>(p);
+}
+
+//---------------------
+void test_per_class_allocator_1()
+{
+	cout << "\ntest_per_class_allocator_1()................ \n";
+	cout << sizeof(Screen) << endl;
+	//pi 下N太大会造成crash
+	size_t const N = 20;
+	Screen *p[N];
+	for (int i=0; i<N; ++i) {
+		p[i] = new Screen(i);
+	}
+	
+	//输出前10个pointers，用以比较其间隔
+	for (int i=0; i<10; ++i) {
+		cout << p[i] << endl;
+	}
+	//result: 间隔为8，中间没有cookie
+		//0x114010
+		//0x114018
+		//0x114020
+		//0x114028
+		//0x114030
+		//0x114038
+		//0x114040
+		//0x114048
+		//0x114050
+		//0x114058
+
+	for (int i=0; i<N; ++i) {
+		delete p[i];
+	}
+}
+}
+
 int main(int argc, char *argv[])
 {
-	demo01::test_primitives();
-	demo02::test_call_ctor_directory();
+//	demo01::test_primitives();
+//	demo02::test_call_ctor_directory();
+//	demo03::test_array_new_and_placement_new();
+	demo04::test_per_class_allocator_1();
 	return 0;
 }
