3e3b2b2f59bf6539b86e39044943d5e59021bac9
MingYueRuYa <635672377@qq.com>
2017-09-27 15:48:38 +0800

    add test stl container.

--- c++_code/STLlib/stl.cpp
+++ c++_code/STLlib/stl.cpp
@@ -18,6 +18,8 @@
 
 const long ASIZE = 500000L;
 
+#define SECONDS(mseconds) ((mseconds)*1.0/CLOCKS_PER_SEC)
+
 using std::cin;
 using std::cout;
 using std::endl;
@@ -94,6 +96,7 @@ namespace test_array
 
 #include <vector>
 #include <stdexcept>
+#include <algorithm>
 
 using std::vector;
 using std::string;
@@ -103,6 +106,7 @@ namespace test_vector
 {
 	void test_vector()
 	{
+		std::cout << "............test vector............" << std::endl;
 		srand(time(nullptr));
 
 		vector<string> c;
@@ -125,24 +129,221 @@ namespace test_vector
 		cout << "vector.front()= " << c.front() << endl;
 		cout << "vector.back()= " << c.back() << endl;
 		cout << "vector.data()= " << c.data() << endl;
+
+		string target = get_a_target_string();
+		{
+			timestart = clock();
+			auto item = find(c.begin(), c.end(), target);
+			cout << "std::find(), seconds:" << SECONDS(clock()-timestart) << endl;
+			if (c.end() != item) { cout << "found, " << *item << endl; }
+			else { cout << "not found! " << endl; }
+		}
+
+		{
+			timestart = clock();
+			sort(c.begin(), c.end());
+			cout << "sort(), seconds:" << SECONDS(clock()-timestart) << endl;
+			timestart = clock();
+			string *item = (string *)::bsearch(&target, (c.data()), c.size(), sizeof(string), compareString);
+			//cout << "bsearch, seconds:" << SECONDS(clock()-timestart) << endl;
+			cout << "bsearch, seconds:" << std::fixed << SECONDS(clock()-timestart) << endl; //std::fixed 不使用科学计数法
+			if (nullptr != item) { cout << "found, " << *item << endl; }
+			else { cout << "not found! " << endl; }
+		}
+		
 		//result:
-//			seconds: 0.259996
-//			vector.size()= 1000000
-//			vector.max_size()= 1073741823 //实际大小
-//			vector.front()= 187158322
-//			vector.back()= 128384975
-//			vector.data()= 0xb6c95008
+		//............test vector............
+		//seconds: 0.259996
+		//vector.size()= 1000000
+		//vector.max_size()= 1073741823 //实际大小
+		//vector.front()= 187158322
+		//vector.back()= 128384975
+		//vector.data()= 0xb6c95008
 		//! 当size为1,000,000时插入时间为2.9s
 		//! 当size为10,000,000时直接killed
 
+		//target (0~2147483647): std::find(), seconds:0.017957
+		//not found! 
+		//sort(), seconds:1.26775
+		//bsearch, seconds:0.000005
+		//not found! 
 	}
 
+};
+
+#include <list>
+using std::list;
+
+namespace test_list
+{
+	void test_list()
+	{
+		std::cout << "............test list............" << std::endl;
+		list<string> c;
+
+		char buf[10];
+		clock_t timestart = clock();
+
+		for (int i=0; i<1000000; ++i) {
+			try {
+				snprintf(buf, 10, "%d", rand());
+				c.push_back(string(buf));
+			}
+			catch(exception &p) {
+				cout << "i=" << i << " " << p.what() << endl;
+				abort();
+			}
+		} //for 
+
+		cout << "seconds: " << (double)(clock()-timestart)/CLOCKS_PER_SEC << endl;
+		cout << "list.size()= " << c.size() << endl;
+		cout << "list.max_size()= " << c.max_size() << endl;
+		cout << "list.front()= " << c.front() << endl;
+		cout << "list.back()= " << c.back() << endl;
+
+		string target = get_a_target_string();
+		{
+			timestart = clock();
+			auto item = find(c.begin(), c.end(), target);
+			cout << "std::find(), seconds:" << SECONDS(clock()-timestart) << endl;
+			if (c.end() != item) { cout << "found, " << *item << endl; }
+			else { cout << "not found! " << endl; }
+		}
+
+		{
+			timestart = clock();
+			c.sort();
+			cout << "sort(), seconds:" << SECONDS(clock()-timestart) << endl;
+		}
+
+		//result:
+		//............test list............
+		//seconds: 0.366951
+		//list.size()= 1000000
+		//list.max_size()= 357913941
+		//list.front()= 180428938
+		//list.back()= 429357853
+		//target (0~2147483647): std::find(), seconds:0.022652
+		//not found! 
+		//sort(), seconds:1.29558
+	}
+};
+
+#include <forward_list>
+
+using std::forward_list;
+
+namespace test_forward_list
+{
+	void test_forward_list()
+	{
+		std::cout << "............test forward list............" << std::endl;
+		srand(time(nullptr));
+
+		forward_list<string> c;
+		char buf[10];
+		clock_t timestart = clock();
+
+		for (int i=0; i<1000000; ++i) {
+			try {
+				snprintf(buf, 10, "%d", rand());
+				c.push_front(string(buf));
+			}
+			catch(exception &p) {
+				cout << "i=" << i << " " << p.what() << endl;
+				abort();
+			}
+		} //for
+
+		cout << "seconds: " << (double)(clock()-timestart)/CLOCKS_PER_SEC << endl;
+		cout << "forward_list.max_size()= " << c.max_size() << endl;
+		string target = get_a_target_string();
+		timestart = clock();
+		auto item = std::find(c.begin(), c.end(), target);
+		cout << "std::find(), seconds:" << SECONDS(clock()-timestart) << endl;
+		if (c.end() != item) { cout << "found, " << *item << endl; }
+		else { cout << "not found! " << endl; }
+
+		{
+			timestart = clock();
+			c.sort();
+			cout << "sort(), seconds:" << SECONDS(clock()-timestart) << endl;
+		}
+		//result:
+		//............test forward list............
+		//seconds: 0.333147
+		//forward_list.max_size()= 536870911
+		//target (0~2147483647): std::find(), seconds:0.044755
+		//not found! 
+		//sort(), seconds:1.54503
+	}
+};
+
+#include <deque>
+
+using std::deque;
+
+namespace test_deque
+{
+	void test_deque()
+	{
+		std::cout << "............test deque............" << std::endl;
+		deque<string> c;
+
+		char buf[10];
+		clock_t timestart = clock();
+
+		for (int i=0; i<1000000; ++i) {
+			try {
+				snprintf(buf, 10, "%d", rand());
+				c.push_back(string(buf));
+			}
+			catch(exception &p) {
+				cout << "i=" << i << " " << p.what() << endl;
+				abort();
+			}
+		} //for 
+
+		cout << "seconds: " << (double)(clock()-timestart)/CLOCKS_PER_SEC << endl;
+		cout << "deque.size()= " << c.size() << endl;
+		cout << "deque.max_size()= " << c.max_size() << endl;
+		cout << "deque.front()= " << c.front() << endl;
+		cout << "deque.back()= " << c.back() << endl;
+
+		string target = get_a_target_string();
+		{
+			timestart = clock();
+			auto item = find(c.begin(), c.end(), target);
+			cout << "std::find(), seconds:" << SECONDS(clock()-timestart) << endl;
+			if (c.end() != item) { cout << "found, " << *item << endl; }
+			else { cout << "not found! " << endl; }
+		}
+
+		{
+			timestart = clock();
+			sort(c.begin(), c.end());
+			cout << "sort(), seconds:" << SECONDS(clock()-timestart) << endl;
+		}
+		//result:
+			//............test deque............
+			//seconds: 0.234153
+			//deque.size()= 1000000
+			//deque.max_size()= 1073741823
+			//deque.front()= 180428938
+			//deque.back()= 429357853
+			//target (0~2147483647): std::find(), seconds:0.013567
+			//not found! 
+			//sort(), seconds:1.35966
+	}
 };
 
 int main(int argc, char *argv[])
 {
 	//test_array::test_array();
-	test_vector::test_vector();
+	//test_vector::test_vector();
+	//test_list::test_list();
+	//test_forward_list::test_forward_list();
+	test_deque::test_deque();
 	return 0;
 }
 
