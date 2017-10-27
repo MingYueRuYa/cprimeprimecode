9dbd5d9f3818005dd1b37b3f110624ca35940b83
MingYueRuYa <635672377@qq.com>
2017-10-27 10:00:44 +0800

    add test vector insert_range function.

--- stl_source_code/main.cpp
+++ stl_source_code/main.cpp
@@ -15,18 +15,31 @@ public:
 		mID = pObj.mID;
 		cout << "object copy ctor... id : " << mID << endl; 
 	}
-	virtual ~Object() { cout << "~object dtor... id: " << mID << endl; }
+
+	Object& operator=(const Object &pObj) {
+		mID = pObj.mID;
+		cout << "operator= object... id : " << mID << endl; 
+		return *this;
+	}
+
+	virtual ~Object() { 
+		cout << "~object dtor... id: " << mID << endl; 
+	}
 
 	virtual void Show() { 
 		cout << std::hex << this << std::dec << " id: " << mID; 
 	}
 
-private:
+public:
 	int mID;
 };
 
 void test_vector()
 {
+//#define TEST_VECTOR_SWAP
+//#define TEST_VECTOR_POP_ERASE
+//#define TEST_VECTOR_INSERT
+
 	vector<int> vec;
 	for (size_t i=0; i<10; ++i) {
 		vec.insert(vec.begin(), i);
@@ -51,6 +64,7 @@ void test_vector()
 	}
 	cout << endl;
 
+#	ifdef TEST_VECTOR_SWAP
 	cout << "-----------------behind swap--------------------" << endl;
 	vec.swap(vec2);	
 
@@ -64,7 +78,9 @@ void test_vector()
 	}
 	cout << endl;
 	cout << "-----------------behind swap--------------------" << endl;
+#	endif //TEST_VECTOR_SWAP	
 
+#	ifdef TEST_VECTOR_POP_ERASE
 	//自定义对象（no-trivial constructor）
 	//如果每次capacity扩展时，都会调用copy constructor
 	vector<Object> vec3;
@@ -104,8 +120,37 @@ void test_vector()
 		vec3 = vec4;
 		cout << "--------------behind operator= -----------------" << endl;
 	}
+#	endif //TEST_VECTOR_POP_ERASE
 
-	
+#		ifdef TEST_VECTOR_INSERT
+	{ //test insert_range
+		cout << "------------before insert_range ----------------" << endl;
+		vector<Object> objvec;
+		//插入1-5，此时caption应该为8
+		for (int i=0; i<5; i++) {
+			cout << "-----objvec capacity " << objvec.capacity() 
+			     << "-----" << endl;
+			objvec.push_back(Object(i));
+		}
+		vector<Object> objvec2;
+		for (int i=5; i<7; i++) {
+			cout << "-----objvec2 capacity " << objvec2.capacity() 
+			     << "-----" << endl;
+			objvec2.push_back(Object(i));
+		}
+
+		cout << "------------- start insert range -------------" << endl;
+		objvec.insert(objvec.end()-1, objvec2.begin(), objvec2.end());
+		cout << "------------- end  insert  range -------------" << endl;
+
+		for (int i=0; i<7; i++) {
+			cout << objvec[i].mID << " ";
+		}
+		cout << endl;
+
+		cout << "------------behind insert_range ----------------" << endl;
+	}
+#		endif //TEST_VECTOR_INSERT	
 }
 
 int main(int argc, char *argv[])
