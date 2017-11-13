914aeb067ed722bb1612b86c56041e78beb3cd07
MingYueRuYa <635672377@qq.com>
2017-11-13 10:09:53 +0800

    add push status for git commit tool sh.

--- git_commit_pull_push.sh
+++ git_commit_pull_push.sh
@@ -12,3 +12,10 @@ git commit -m "$1"
 
 echo -e "\e[4m\e[1;32mgit push\e[0m"
 git push
+
+if [[ $? == 0  ]];then
+    echo -e "\e[4m\e[1;32mpush successful.\e[0m"
+else
+    echo -e "\e[4m\e[1;31mpush failed\e[0m"
+fi
+
