52afd099d9b0262a915d46a9fb14639f75d403b8
MingYueRuYa <635672377@qq.com>
2017-11-13 10:07:26 +0800

    git commit tool sh add font color.

--- git_commit_pull_push.sh
+++ git_commit_pull_push.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo "git pull..."
+echo -e "\e[4m\e[1;32mgit pull...\e[0m"
 git pull
 
 if [ $# -eq 0 ]; then
     exit 0 
 fi
 
-echo "git commit -m $1"
+echo -e "\e[4m\e[1;32mgit commit -m $1\e[0m"
 git commit -m "$1"
 
-echo "git commit -m $1"
+echo -e "\e[4m\e[1;32mgit push\e[0m"
 git push
