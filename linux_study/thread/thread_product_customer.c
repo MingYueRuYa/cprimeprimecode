6dd1dcca8df8ca6052458d022f9c49c92812ae8d
MingYueRuYa <635672377@qq.com>
2017-12-22 22:08:18 +0800

    finish thread product custom.

--- linux_study/thread/thread_product_customer.c
+++ linux_study/thread/thread_product_customer.c
@@ -13,13 +13,14 @@
 #include <errno.h>
 #include <semaphore.h>
 
-#define CUSTOMER_COUNT 	 5
-#define PRODUCTER_COUNT  1
+#define CUSTOMER_COUNT 	 1
+#define PRODUCTER_COUNT  5
 #define BUFF_SIZE		 10
 
 sem_t g_sem_full;
 sem_t g_sem_empty;
-pthread_mutex_t g_mutex;
+pthread_mutex_t g_mutex_product;
+pthread_mutex_t g_mutex_custom;
 
 int g_buf[BUFF_SIZE];
 
@@ -30,72 +31,76 @@ int custom_id	= 0;
 
 void *product(void *arg)
 {
-	//是否已满
-	sem_wait(&g_sem_full);
-	//临界区
-	pthread_mutex_lock(&g_mutex);
+    while (1) {
+        //是否已满
+        sem_wait(&g_sem_full);
+        //临界区
+        pthread_mutex_lock(&g_mutex_product);
 
-	int i = 0;
-	for (; i<BUFF_SIZE; ++i) {
-		printf("%02d ", i);
-		if (g_buf[i] == -1) {
-			printf("%s", "NULL");
-		} else {
-			printf("%d", g_buf[i]);
-		}
-	
-		if (i == in) {
-			printf("\t<--produce");
-		}
-		printf("\n");
-	}
-	
-	printf("begin produce product %d\n", product_id);
-	g_buf[in] = product_id++;
-	in = (in+1)%BUFF_SIZE;
+        int i = 0;
+        for (; i<BUFF_SIZE; ++i) {
+            printf("%02d ", i);
+            if (g_buf[i] == -1) {
+                printf("%s", "NULL");
+            } else {
+                printf("%d", g_buf[i]);
+            }
+        
+            if (i == in) {
+                printf("\t<--produce");
+            }
+            printf("\n");
+        }
+        
+        printf("begin produce product %d\n", product_id);
+        g_buf[in] = product_id++;
+        in = (in+1)%BUFF_SIZE;
 
-	sleep(1);
-	pthread_mutex_unlock(&g_mutex);
-	//发送不为空信号量
-	sem_post(&g_sem_empty);
+        sleep(1);
+        pthread_mutex_unlock(&g_mutex_product);
+        //发送不为空信号量
+        sem_post(&g_sem_empty);
+    }
 	return NULL;
 }	
 
 void *custom(void *arg)
 {
-	//是否为空
-	sem_wait(&g_sem_empty);
-	//临界区
-	pthread_mutex_lock(&g_mutex);
+    while (1) {
+        //是否为空
+        sem_wait(&g_sem_empty);
+        //临界区
+        pthread_mutex_lock(&g_mutex_custom);
 
-	int i = 0;
-	for (; i<BUFF_SIZE; ++i) {
-		printf("%02d ", i);
-		if (g_buf[i] == -1) {
-			printf("%s", "NULL");
-		} else {
-			printf("%d", g_buf[i]);
-		}
-	
-		if (i == out) {
-			printf("\t-->custom");
-		}
-		printf("\n");
-	}
+        int i = 0;
+        for (; i<BUFF_SIZE; ++i) {
+            printf("%02d ", i);
+            if (g_buf[i] == -1) {
+                printf("%s", "NULL");
+            } else {
+                printf("%d", g_buf[i]);
+            }
+        
+            if (i == out) {
+                printf("\t-->custom");
+            }
+            printf("\n");
+        }
 
-	custom_id = g_buf[out];
-	printf("begin consume product %d\n", custom_id);
-	g_buf[out] = -1;
-	out = (out+1)%BUFF_SIZE;
-	printf("end consume product %d\n", custom_id);
-	sleep(3);
-	pthread_mutex_unlock(&g_mutex);
-	//发送不为满信号量
-	sem_post(&g_sem_full);
+        custom_id = g_buf[out];
+        printf("begin consume product %d\n", custom_id);
+        g_buf[out] = -1;
+        out = (out+1)%BUFF_SIZE;
+        printf("end consume product %d\n", custom_id);
+        sleep(3);
+        pthread_mutex_unlock(&g_mutex_custom);
+        //发送不为满信号量
+        sem_post(&g_sem_full);
+    }
 	return NULL;
 }	
 
-pthread_t tid[CUSTOMER_COUNT+PRODUCTER_COUNT] = {-1};
+pthread_t tid[CUSTOMER_COUNT+PRODUCTER_COUNT];
 
 int main(int argc, char *argv[])
 {
@@ -106,7 +111,8 @@ int main(int argc, char *argv[])
 
 	sem_init(&g_sem_full, 0, BUFF_SIZE);
 	sem_init(&g_sem_empty, 0, 0);
-	pthread_mutex_init(&g_mutex, NULL);
+	pthread_mutex_init(&g_mutex_product, NULL);
+	pthread_mutex_init(&g_mutex_custom, NULL);
 
 	for (i=0; i<CUSTOMER_COUNT; ++i) {
 		pthread_create(&tid[i], NULL, custom, (void *)i);	
@@ -124,15 +130,17 @@ int main(int argc, char *argv[])
 	printf("\n");
 
 	i = 0;
-// 	for (; i<CUSTOMER_COUNT+PRODUCTER_COUNT; ++i) {
-// 		pthread_join(tid[i], NULL);
-// 	}
+ 	for (; i<CUSTOMER_COUNT+PRODUCTER_COUNT; ++i) {
+ 		pthread_join(tid[i], NULL);
+ 	}
+    printf("\n");
 
-	sleep(10);
+	// sleep(10);
 
 	sem_destroy(&g_sem_empty);	
 	sem_destroy(&g_sem_full);	
-	pthread_mutex_destroy(&g_mutex);
+	pthread_mutex_destroy(&g_mutex_product);
+	pthread_mutex_destroy(&g_mutex_custom);
 
 	return 0;
 }
