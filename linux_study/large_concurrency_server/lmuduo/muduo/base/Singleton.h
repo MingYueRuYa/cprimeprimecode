d41ad3c8ed359f17061dd2998df419804875bcf4
MingYueRuYa <635672377@qq.com>
2018-01-09 21:23:01 +0800

    add muduo singleton and BoundedBlockingQueue

---
+++ linux_study/large_concurrency_server/lmuduo/muduo/base/Singleton.h
@@ -0,0 +1,60 @@
+// Use of this source code is governed by a BSD-style license
+// that can be found in the License file.
+//
+// Author: Shuo Chen (chenshuo at chenshuo dot com)
+
+#ifndef MUDUO_BASE_SINGLETON_H
+#define MUDUO_BASE_SINGLETON_H
+
+#include <boost/noncopyable.hpp>
+#include <pthread.h>
+#include <stdlib.h> // atexit
+
+namespace muduo
+{
+
+template<typename T>
+class Singleton : boost::noncopyable
+{
+ public:
+  static T& instance()
+  {
+	  // 这个函数只执行一次
+    pthread_once(&ponce_, &Singleton::init);
+    return *value_;
+  }
+
+ private:
+  Singleton();
+  ~Singleton();
+
+  static void init()
+  {
+    value_ = new T();
+    ::atexit(destroy);
+  }
+
+  static void destroy()
+  {
+	// 判断是否是不完全类型
+    typedef char T_must_be_complete_type[sizeof(T) == 0 ? -1 : 1];
+    delete value_;
+	// 不允许delete不完全类型，因为delete 不完全有安全隐患
+	// 不会调用析构函数
+	// 直接调用::operator delete 释放内存
+  }
+
+ private:
+  static pthread_once_t ponce_;
+  static T*             value_;
+};
+
+template<typename T>
+pthread_once_t Singleton<T>::ponce_ = PTHREAD_ONCE_INIT;
+
+template<typename T>
+T* Singleton<T>::value_ = NULL;
+
+}
+#endif
+
