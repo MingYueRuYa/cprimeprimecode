3c54a2a53dd6a43d5428477b41a197c1dd2c966f
MingYueRuYa <635672377@qq.com>
2018-09-28 23:10:18 +0800

    enhance unzip.sh code.

--- sh/unzip.sh
+++ sh/unzip.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-zipname=$1
-unzip -OGBK ${zipname} -d ${zipname%.*}
+zipname=$1 
+if [ $# -eq 1 ]; then
+   dirname=${zipname%.*}
+else 
+    dirname=$2
+fi
+echo ${zipname}
+unzip -OGBK "${zipname}" -d ${dirname}
