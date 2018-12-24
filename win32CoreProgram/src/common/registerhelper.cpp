4a6459ef47bf5f5e46af2430b327fd591d0817b8
liushixiong <liushixiong@2144.cn>
2018-12-24 19:47:51 +0800

    完善注册表内容增加遍历key和遍历value

--- win32CoreProgram/src/common/registerhelper.cpp
+++ win32CoreProgram/src/common/registerhelper.cpp
@@ -7,6 +7,7 @@
 
 #include "stdafx.h"
 
+#include "debughelper.h"
 #include "registerhelper.h"
 
 #include <stdio.h>
@@ -74,6 +75,57 @@ RegisterHelper::~RegisterHelper()
 {
 }
 
+DWORD RegisterHelper::SetDWORD(const wstring &keyName, DWORD dwValue)
+{
+	return SetValue(REG_DWORD, keyName, dwValue);
+}
+
+DWORD RegisterHelper::GetDWORD(const wstring &keyName, DWORD &dwValue)
+{
+	return GetValue(REG_DWORD, keyName, dwValue);
+}
+
+DWORD RegisterHelper::SetQWORD(const wstring &keyName, int64_t dwValue)
+{
+	return SetValue(REG_QWORD, keyName, dwValue);
+}
+
+DWORD RegisterHelper::GetQWORD(const wstring &keyName, int64_t &dwValue)
+{
+	return GetValue(REG_QWORD, keyName, dwValue);
+}
+
+DWORD RegisterHelper::SetSZ(const wstring &keyName, const wstring &strValue)
+{
+	return SetValue(REG_SZ, keyName, strValue);
+}
+
+DWORD RegisterHelper::GetSZ(const wstring &keyName, wstring &strValue)
+{
+	return GetValue(REG_SZ, keyName, strValue);
+}
+
+DWORD RegisterHelper::SetMultiSZ(const wstring &keyName, 
+									const wstring &strValue)
+{
+	return SetValue(REG_MULTI_SZ, keyName, strValue);
+}
+
+DWORD RegisterHelper::GetMultiSZ(const wstring &keyName, wstring &strValue)
+{
+	return GetValue(REG_MULTI_SZ, keyName, strValue);
+}
+
+DWORD RegisterHelper::SetExpandSZ(const wstring &keyName, const wstring &strValue)
+{
+	return SetValue(REG_EXPAND_SZ, keyName, strValue);
+}
+
+DWORD RegisterHelper::GetExpandSZ(const wstring &keyName, wstring &strValue)
+{
+	return GetValue(REG_EXPAND_SZ, keyName, strValue);
+}
+
 DWORD RegisterHelper::DeleteValue(const wstring &keyName)
 {
 	if (keyName.empty()) { return -1; }
@@ -121,15 +173,85 @@ DWORD RegisterHelper::DeleteKey()
 	DWORD errorcode = RegOpenKeyExW(mRootKey, partialpath.c_str(), 0, 
 											mSamDesired, &hKey);
 	if (ERROR_SUCCESS != errorcode) { return errorcode; }
+
 	errorcode = RegDeleteKeyExW(hKey, keyname.c_str(), mSamDesired, 0);
 	if (ERROR_SUCCESS != errorcode) { 
 		RegCloseKey(hKey);
 		return errorcode; 
 	}
-	RegCloseKey(hKey);
 
+	RegCloseKey(hKey);
 	return errorcode;
 }
+
+DWORD RegisterHelper::TraverseKey()
+{
+	HKEY hKey = 0;
+	DWORD errorcode = RegOpenKeyExW(mRootKey, mSubPath.c_str(), 0, 
+									mSamDesired, &hKey);
+	if (ERROR_SUCCESS != errorcode) { return errorcode; }
+	DWORD dwIndex = 0;
+	do {
+		wchar_t lpName[MAX_PATH]	= {0};
+		DWORD cchName				= MAX_PATH;
+		errorcode = RegEnumKeyExW(hKey, dwIndex, lpName, &cchName, 
+									NULL, NULL, NULL, NULL);
+		if (errorcode != ERROR_SUCCESS) { break; }
+		
+		dwIndex++;
+#ifdef XIBAO_DEBUG_HELPER
+		DebugHelper::OutputDebugStringW(wstring(lpName) + L"\r\n");
+#endif // XIBAO_DEBUG_HELPER
+	} while (errorcode != ERROR_NO_MORE_ITEMS);
+
+	if (errorcode == ERROR_NO_MORE_ITEMS) { errorcode = ERROR_SUCCESS; }
+
+	RegCloseKey(hKey);	
+	return errorcode;
+}
+
+DWORD RegisterHelper::TraverseValue()
+{
+	HKEY hKey = 0;
+	DWORD errorcode = RegOpenKeyExW(mRootKey, mSubPath.c_str(), 0, 
+									mSamDesired, &hKey);
+	DWORD dwIndex = 0;
+	do {
+		wchar_t wchvalue[MAX_PATH] = {0};
+		DWORD cchcount = MAX_PATH; 
+		DWORD type = 0;
+		errorcode = RegEnumValueW(hKey, dwIndex, wchvalue, &cchcount, 
+									0, 0, 0, 0);
+		if (errorcode != ERROR_SUCCESS) { break; }
+
+		DWORD cbData = MAX_PATH;
+		// TODO:这里需要不断的分配内存可以优化
+		BYTE *lpData = new BYTE[MAX_PATH];
+		errorcode = RegQueryValueExW(hKey, wchvalue, 0, &type, lpData, &cbData);
+		if (errorcode != ERROR_SUCCESS) { continue; }
+
+		// TODO: 这里的if判断考虑用创建RegDataType工厂，消除
+		wstring value = L"";
+		DWORD dwvalue = 0;
+		if (type == REG_SZ || type == REG_EXPAND_SZ || type == REG_MULTI_SZ) {
+			value = reinterpret_cast<wchar_t *>(lpData);	
+		} else if (type == REG_DWORD) {
+			dwvalue = *(reinterpret_cast<DWORD *>(lpData));	
+		}
+		
+		dwIndex++;
+#ifdef XIBAO_DEBUG_HELPER
+		DebugHelper::OutputDebugStringW(L"%s:%s\r\n", wchvalue, value.c_str());
+#endif // XIBAO_DEBUG_HELPER
+		delete [] lpData;
+	} while(errorcode != ERROR_NO_MORE_ITEMS);
+
+	if (errorcode == ERROR_NO_MORE_ITEMS) { errorcode = ERROR_SUCCESS; }
+
+	RegCloseKey(hKey);	
+	return errorcode;
+}
+
 
 void RegisterHelper::_CopyValue(const RegisterHelper &right)
 {
--- win32CoreProgram/src/common/registerhelper.h
+++ win32CoreProgram/src/common/registerhelper.h
@@ -8,12 +8,18 @@
 #ifndef registerhelper_h
 #define registerhelper_h
 
+#include "utili.hpp"
+
 #include <map>
 #include <string>
+#include <memory>
 #include <windows.h>
 
 using std::map;
 using std::wstring;
+using std::shared_ptr;
+using std::make_shared;
+using std::make_shared_array;
 
 namespace XIBAO {
 
@@ -47,23 +53,54 @@ private:
 		public:
 			explicit RegDataType(DWORD regType, const T &tValue)
 				:	mRegType(regType),
-					mValue(tValue)
+					mValue(tValue),
+					mByteSize(MAX_PATH*2)
 			{ 
 				// 静态断言
-				static_assert(! std::is_pointer<T>::value, "Not support pinter type.");
+				static_assert(! std::is_pointer<T>::value, 
+								"Not support pinter type.");
+			}
+
+			explicit RegDataType(DWORD regType)
+				:	mRegType(regType),
+					mValue(T()),
+					mByteSize(MAX_PATH*2)
+			{ 
+				// 静态断言
+				static_assert(! std::is_pointer<T>::value, 
+								"Not support pinter type.");
 			}
 
 			~RegDataType() {}
 			RegDataType(const RegDataType &right) = default;
 			RegDataType& operator=(const RegDataType &right) = default;
 
-			unsigned int size() const { return sizeof(T); }
+			unsigned int ValueSize() const { return sizeof(T); }
 			const T *GetAddress() const { return &mValue; }
 			DWORD GetType() const { return mRegType; }
+			DWORD *GetType() { return &mRegType; }
+			
+			BYTE *GetByte()
+			{
+				if (nullptr == mshByte.get()) {
+					mshByte = make_shared<BYTE>(mByteSize);
+				} 
+				return mshByte.get();
+			}
+
+			DWORD *GetByteSize() { return &mByteSize; }
+
+			T GetValue() 
+			{
+				mValue = *(reinterpret_cast<T *>(mshByte.get()));
+				return mValue;
+			}
 
 		public:
 			DWORD mRegType;
+			DWORD mByteSize;
 			T	  mValue;
+			shared_ptr<BYTE> mshByte;
 	};
 
 	template <>
@@ -71,7 +108,15 @@ private:
 	public:
 		RegDataType(DWORD regType, const wstring &tValue)
 			: mRegType(regType),
-			  mValue(tValue)
+			  mValue(tValue),
+			  mByteSize(MAX_PATH*2)
+		{ 
+		}
+
+		RegDataType(DWORD regType)
+			: mRegType(regType),
+			  mValue(wstring(L"")),
+			  mByteSize(MAX_PATH*2)
 		{ 
 		}
 
@@ -80,13 +125,32 @@ private:
 		RegDataType(const RegDataType &right) = default;
 		RegDataType& operator=(const RegDataType &right) = default;
 
-		unsigned int size() const { return mValue.length() * sizeof(wchar_t); }
+		unsigned int ValueSize() const 
+		{ return mValue.length() * sizeof(wchar_t); }
 		const wchar_t *GetAddress() const { return mValue.c_str(); }
 		DWORD GetType() const { return mRegType; }
+
+		wchar_t *GetByte()
+		{
+			if (nullptr == mshByte.get()) {
+				mshByte = make_shared_array<wchar_t>(mByteSize);
+			} 
+			return mshByte.get();
+		}
+
+		DWORD *GetByteSize() { return &mByteSize; }
+
+		wstring GetValue() 
+		{
+			mValue = wstring(mshByte.get());
+			return mValue;
+		}
 
 	public:
 		DWORD	mRegType;
 		wstring	mValue;
+		DWORD mByteSize;
+		shared_ptr<wchar_t> mshByte;
 	};
 
 public:
@@ -95,55 +159,63 @@ public:
 	RegisterHelper& operator=(const RegisterHelper &right);
 	~RegisterHelper();
 
+	DWORD SetDWORD(const wstring &keyName, DWORD dwValue);
+	DWORD GetDWORD(const wstring &keyName, DWORD &dwValue);
+
+	DWORD SetQWORD(const wstring &keyName, int64_t dwValue);
+	DWORD GetQWORD(const wstring &keyName, int64_t &dwValue);
+
+	DWORD SetSZ(const wstring &keyName, const wstring &strValue);
+	DWORD GetSZ(const wstring &keyName, wstring &strValue);
+
+	DWORD SetMultiSZ(const wstring &keyName, const wstring &strValue);
+	DWORD GetMultiSZ(const wstring &keyName, wstring &strValue);
+
+	DWORD SetExpandSZ(const wstring &keyName, const wstring &strValue);
+	DWORD GetExpandSZ(const wstring &keyName, wstring &strValue);
+
+	DWORD DeleteValue(const wstring &keyName);
+	DWORD DeleteAllValues();
+	DWORD DeleteKey();
+	DWORD TraverseKey();
+	DWORD TraverseValue();
+
+private:
 	template<typename TRetValue>
-	DWORD GetValue(const wstring &keyName, TRetValue &value)
+	DWORD GetValue(DWORD regType, const wstring &keyName, TRetValue &value)
 	{
-		HKEY hKey = 0;
-		DWORD result = NOERROR;
+		HKEY hKey		= 0;
+		DWORD result	= NOERROR;
+
+		RegDataType<TRetValue> regdata(regType);
 
 		long lRet = RegOpenKeyExW(mRootKey, mSubPath.c_str(), 0,
-			mSamDesired, &hKey);
+									mSamDesired, &hKey);
 		if (lRet != ERROR_SUCCESS) {
 			return GetLastError();
 		}
 
-		// 1024
 		DWORD regtype;
-		DWORD count		= MAX_PATH*4;
-		BYTE *lpData	= new BYTE[MAX_PATH*4];
-		lRet			= RegQueryValueExW(hKey, 
-											keyName.c_str(), 
-											NULL, 
-											&regtype, 
-											lpData, 
-											&count);
+		lRet = RegQueryValueExW(hKey, 
+								keyName.c_str(), 
+								NULL, 
+								&regtype, 
+								(BYTE *)regdata.GetByte(),
+								regdata.GetByteSize()
+								);
 		
 		if (lRet != ERROR_SUCCESS) {
-			delete[] lpData;
 			return GetLastError();
 		}
-
-		
-		// TODO 需要优化下此处的if语句
-		if (REG_SZ == regtype || REG_EXPAND_SZ == regtype ||
-			REG_MULTI_SZ == regtype) {
-			value = TRetValue((reinterpret_cast<wchar_t *>(lpData)));
-		} else if (REG_DWORD == regtype || 
-					REG_DWORD_LITTLE_ENDIAN == regtype ||
-					REG_DWORD_BIG_ENDIAN == regtype || 
-					REG_QWORD == regtype ||
-					REG_DWORD_BIG_ENDIAN == regtype) {
-			value = *(reinterpret_cast<TRetValue *>(lpData));
-		}
-
-		delete[] lpData;
+		value = regdata.GetValue();
 		return 0;
 	}
 
 	template<typename TRetValue>
-	DWORD SetValue(const wstring &keyName, 
-					const TRetValue &value,
-					DWORD regType)
+	DWORD SetValue(DWORD regType,
+					const wstring &keyName,
+					const TRetValue &value
+					)
 	{
 		RegDataType<TRetValue> datatype(regType, value);
 
@@ -162,10 +234,10 @@ public:
 			}
 		}
 
-		lpData = new BYTE[datatype.size()];
-		CopyMemory(lpData, datatype.GetAddress(), datatype.size());
-		lRet = RegSetValueEx(hKey, keyName.c_str(), 0, 
-								datatype.GetType(), lpData, datatype.size());
+		lpData = new BYTE[datatype.ValueSize()];
+		CopyMemory(lpData, datatype.GetAddress(), datatype.ValueSize());
+		lRet = RegSetValueEx(hKey, keyName.c_str(), 0, datatype.GetType(), 
+								lpData, datatype.ValueSize());
 		result = lRet;
 		RegCloseKey(hKey);
 		delete[] lpData;
@@ -176,10 +248,6 @@ public:
 		delete[] lpData;
 		return result;
 	}
-
-	DWORD DeleteValue(const wstring &keyName);
-	DWORD DeleteAllValues();
-	DWORD DeleteKey();
 
 private:
 	void _CopyValue(const RegisterHelper &right);
