24431718cf143dfaf10de79cf6837ceebb738bb8
MingYueRuYa <635672377@qq.com>
2019-01-15 23:04:00 +0800

    add thread project

--- win32CoreProgram/src/dump/main.cpp
+++ win32CoreProgram/src/dump/main.cpp
@@ -7,10 +7,10 @@
 //我们的回调函数  
 LONG __stdcall ExceptCallBack(EXCEPTION_POINTERS *pExcPointer)
 {
-    MessageBox(NULL, _T("程序崩溃！相关信息记录在D:\\Test.dmp文件中。"), NULL, MB_OK);
+    MessageBox(NULL, _T("程序崩溃！相关信息记录在E:\\Test.dmp文件中。"), NULL, MB_OK);
 
     //创建dump文件  
-    HANDLE hFile = CreateFile(_T("D:\\Test.dmp"), GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+    HANDLE hFile = CreateFile(_T("E:\\Test.dmp"), GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
 
     //向文件写下当前程序崩溃相关信息  
     MINIDUMP_EXCEPTION_INFORMATION loExceptionInfo;
